7e3e2e0534f6a6664c75c0ff54056e87ae1aed32
shomy <18222333186@163.com>
2016-08-30 23:18:04 +0800

    del some unused var

--- Python/3.py
+++ Python/3.py
@@ -9,12 +9,11 @@ class Solution(object):
         """
         if not s: return 0
         m = {}
-        l = r = res = 0
+        l  = res = 0
         for i, c in enumerate(s):
-            r = i
             if c in m:
-                if  r - l > res: res = r - l
+                if  i - l > res: res = i - l
                 if l < m[c] + 1: l = m[c] + 1
             m[c] = i
 
-        return max(res,  r - l + 1)
+        return max(res,  i - l + 1)
