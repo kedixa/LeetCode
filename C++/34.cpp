b0ef646c23aaabe56902efb174e1c4487b82f34e
kedixa <1204837541@qq.com>
2016-09-19 13:03:45 +0800

    Search for a Range

---
+++ C++/34.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        auto it1 = lower_bound(nums.begin(), nums.end(), target),
+            it2 = upper_bound(nums.begin(), nums.end(), target);
+        if(it1 == nums.end() || *it1 != target) return vector<int>{-1, -1};
+        else return vector<int>{it1 - nums.begin(), it2 - nums.begin() - 1};
+    }
+};
