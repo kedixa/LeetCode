f4b33cc11f96fcee1193be0740a63e0c1cc32cb1
kedixa <1204837541@qq.com>
2016-09-29 10:22:23 +0800

    N-Queens II

---
+++ C++/52.cpp
@@ -0,0 +1,70 @@
+class Solution {
+    using vvs = vector<vector<string>>;
+    using vvi = vector<vector<int>>;
+    vector<int> answer;
+    void add(vvi &vec, int x, int y, int c)
+    {
+        int sz = vec.size();
+        for(int i = 0; i < sz; ++i)
+        {
+            if(i == y) continue;
+            vec[x][i] += c;
+        }
+        for(int i = 0; i < sz; ++i)
+        {
+            if(i == x) continue;
+            vec[i][y] += c;
+        }
+        int xi, yi;
+        xi = x, yi = y;
+        while(--xi >= 0 && --yi >= 0) vec[xi][yi] += c;
+        xi = x, yi = y;
+        while(--xi >= 0 && ++yi < sz) vec[xi][yi] += c;
+        xi = x, yi = y;
+        while(++xi < sz && ++yi < sz) vec[xi][yi] += c;
+        xi = x, yi = y;
+        while(++xi < sz && --yi >= 0) vec[xi][yi] += c;
+    }
+    void dfs(int &result, vvi &vec, int cur)
+    {
+        /*
+        若最后一行放置完，则保存结果。
+        */
+        if(cur == vec.size())
+        {
+            ++result;
+            return;
+        }
+        /*
+        在当前行找一个可以放置的位置，放置并到下一行查找。
+        */
+        int sz = vec.size();
+        for(int i = 0; i < sz; ++i)
+        {
+            if(vec[cur][i] == 0)
+            {
+                add(vec, cur, i, 1); // 去掉不能放置的位置
+                dfs(result, vec, cur + 1);
+                add(vec, cur, i, -1); // 找回去掉的位置
+            }
+        }
+    }
+public:
+    Solution()
+    {
+        answer.resize(20, -1);
+    }
+    int totalNQueens(int n) {
+        /*
+            递归枚举每个可以放置的位置。
+        */
+        if(answer[n] == -1)
+        {
+            int result = 0;
+            vvi vec(n, vector<int>(n, 0));
+            dfs(result, vec, 0);
+            answer[n] = result;
+        }
+        return answer[n];
+    }
+};
