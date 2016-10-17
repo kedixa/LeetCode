46a689f3a1fa70e251a99eea1d82b8af2d67090d
XinNoil <xinwojiushiwokun@163.com>
2016-10-17 17:30:46 +0800

    py-jk-100,349,383

---
+++ Python-jk/349.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        s1=set(nums1)
+        s2=set(nums2)
+        s=s1&s2
+        return list(s)
